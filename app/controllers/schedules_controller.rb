ddcc5db77775946bc90e82a04299635adc90239d
iwasawa15 <tatsuya57sak@gmail.com>
2019-02-28 18:38:58 +0900

    define create action

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,4 +2,14 @@ class SchedulesController < ApplicationController
   def new
     @schedule = Schedule.new
   end
+
+  def create
+    Schedule.create(schedule_params)
+    redirect_to mypage_path
+  end
+
+  private
+  def schedule_params
+    params.require(:schedule).permit(:name, :date, :place)
+  end
 end
