c67ccb8d100231bce1f97906bad4ea5100eacbfd
iwasawa15 <tatsuya57sak@gmail.com>
2019-02-28 17:01:13 +0900

    add column at schedule

--- db/migrate/20190228075802_create_schedules.rb
+++ db/migrate/20190228075802_create_schedules.rb
@@ -2,6 +2,9 @@ class CreateSchedules < ActiveRecord::Migration[5.2]
   def change
     create_table :schedules do |t|
 
+      t.string :name
+      t.datetime :date, index: true
+      t.string :place
       t.timestamps
     end
   end
