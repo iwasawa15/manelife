8b6377cb75dc2129913b8d8777ab1ed83a93233f
iwasawa15 <tatsuya57sak@gmail.com>
2019-03-07 02:09:51 +0900

    require bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require bootstrap
+//= require moment
+//= require bootstrap-datetimepicker
 //= require activestorage
 //= require turbolinks
 //= require_tree .
