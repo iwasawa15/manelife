dbdf21a4745961b7500de1583fc8a5a2ad78b513
iwasawa15 <tatsuya57sak@gmail.com>
2019-03-13 22:21:05 +0900

    modify style

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,10 +2,11 @@ module HomeHelper
   def menu
     content = "".html_safe
     if user_signed_in?
-      content << link_to("ログアウト", destroy_user_session_path, method: :delete, class: "login_button")
-      content << link_to("マイページ", mypage_path)
+      content << link_to("ログアウト", destroy_user_session_path, method: :delete, class: "menu_button")
+      content << link_to("マイページ", mypage_path, class: "menu_button")
+      content << link_to("スケジュール", schedules_path, class: "menu_button")
     else
-      content << link_to("ログイン", new_user_session_path, class: "login_button")
+      content << link_to("ログイン", new_user_session_path, class: "menu_button")
     end
   end
 end
